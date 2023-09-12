6672a5185c7497c15da87549ecca34ad4578d5b7
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-09-12 20:40:39 +0530

    Time: 75 ms (41.28%) | Memory: 17.6 MB (22.39%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -4,58 +4,26 @@ public:
         int ans=0;
         unordered_map<int,int> charFr;
         map<int,int> frFr;
-        priority_queue<pair<int,int>> q;
 
         for(auto ch: s){
             charFr[ch]++;
         }
 
-
-        // for(auto &cf: charFr){
-        //     while(cf.)
-        // }
-
-
         for(auto cf: charFr){
             frFr[cf.second]++;
         }
-
-        // for(auto &ff: frFr){
 
         for(auto ff=frFr.rbegin(); ff!=frFr.rend(); ff++){
             while(ff->second > 1){
                 ans++;
                 ff->second--;
+
                 if(ff->first > 1)   
                     frFr[ff->first-1]++;
+                    
                 cout<<ff->first<<" "<<ff->second<<endl;
             }
         }
-
-        return ans;
-        // if(charFr.size()==1)
-        //     return 0;
-
-        // if(frFr.size()==1){
-        //     auto key = *frFr.begin();
-        //     if(key.second==2)
-        //         return 1;
-        //     if(key.first>1 && key.second>2)
-        //         return key.second;
-        // }
-
-        // for(auto &ff: frFr){
-        //     while(ff.second>1){
-        //         int cnt = 0, curr = ff.first;
-
-        //         while(frFr.find(curr)!= frFr.end() && curr>0){
-        //             curr--;
-        //             cnt++;
-        //         }
-        //         ff.second--;
-        //         ans += cnt;
-        //     }
-        // }
 
         return ans;
     }
