434418c15a51f838a1d6714f7139e2554613278c
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-09-13 15:13:51 +0530

    Time: 17 ms (62.30%) | Memory: 18.3 MB (15.54%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         // 13.09.23 Potd
-
+        // Approach: Greedy - Two pass
+        
         int ans = 0, n = ratings.size();
-
         vector<int> candies(n, 1);
         int i=1, j=n-2;
 
+        // iterating from left to right to allot candies to children
+        // having higher rating than their left neighbour
         while(i<n){
           if(ratings[i] > ratings[i-1])
             candies[i] = candies[i-1]+1;
@@ -15,13 +17,16 @@ public:
           i++;
         }
 
+        // iterating from right to left to allot candies (if needed) to children
+        // having higher rating than their right neighbour
         while(j>=0){
           if(ratings[j] > ratings[j+1])
             candies[j] = max(candies[j], candies[j+1]+1);
 
           j--;
         }
-    
+
+        // summing up the candies count
         for(auto c: candies)
           ans += c;
 
