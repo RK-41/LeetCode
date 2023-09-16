277cb5c10e06c3ae34d51d1a2536f1713651cc8f
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-09-16 21:33:36 +0530

    Time: 177 ms (78.95%) | Memory: 14.6 MB (90.17%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -8,7 +8,7 @@ public:
         unordered_map<int,int> heap_dict;
 
 
-        // Initializing all distanced to to ininity
+        // Initializing all distanced to ininity
         for(int i=0; i<n; ++i){
             heap_dict[i] = INT_MAX;
         }
@@ -16,15 +16,33 @@ public:
         // Start node
         heap_dict[0] = 0;
 
+        // Priority Queue Initialization:
+        //  'cmp' is a lambda function used to define the comparison function for the priority queue.
+        //  It ensures that the pair with the smallest distance ('left.first') comes at
+        //      the top of the queue.
         auto cmp = [](pair<int,int> left, pair<int,int> right){
             return left.first > right.first;
         };
 
+        // 'min_heap' is a priority queue of pairs, where each pair contains the distance and the node index.
         priority_queue<pair<int,int>, vector<pair<int,int>>, decltype(cmp)>min_heap(cmp);
+        
+        // Initially, the start node(index 0) with a distance of 0 is pushed onto the 'min_heap'.
         min_heap.push({0,0});
 
+        // Minimum Spanning Tree Construction:
         int mst_weight = 0;
 
+        // The loop continues until the 'min_heap' is empty.
+        // It extracts the node 'u' with the smallest distance from the heap.
+        // IF 'u' has already been visited or the distance in 'min_heap' is smaller than 'w',
+        //      it is skipped (to avoid cycles in the MST).
+        // Otherwise, 'u' is marked as visited, and its distance ('w') is added to the MST weight.
+        // For each unvisited node 'v', it calculated the Manhattan distance between 'u' and 'v' 
+        //      using the 'manhattan_dist' function.
+        // If this new distance is smaller that the current distance stored in 'heap_dict' for node 'v',
+        //      the new distance is updated in 'heap_dict', and the pair '(new_distance, v)' is
+        //      pushed onto the 'min_heap'.
         while(!min_heap.empty()){
             auto [w,u] = min_heap.top();
             min_heap.pop();
@@ -46,9 +64,12 @@ public:
             }
         }
 
+        // Finally, the function returns the total weight of the MST.
         return mst_weight;
     }
 
+    // The 'manhattan_dist' function calculates the Manhattan distance b/w tow points 'p1' and 'p2'.
+    // Manhattan distance is the sum of the absolute differences of their x and y coordinates.
     int manhattan_dist(vector<int>& p1, vector<int>& p2){
         return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]);
     }
