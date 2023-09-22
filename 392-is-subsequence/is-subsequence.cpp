f159e302a986046e38ac757dde8ed1ad046f760a
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-09-22 15:38:50 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (12.75%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        // 22.09.23 POTD
+
+        int i=0, j=0;
+
+        while(i<s.size() && j<t.size()){
+            if(s[i]==t[j]){
+                i++, j++;
+            }
+            else
+                j++;
+        }
+
+        if(i==s.size())
+            return true;
+
+        return false;
+    }
+};
