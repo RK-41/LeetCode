db36293f46da8917e597868bb3397bc465eb1886
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-09-25 16:22:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (8.68%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -3,12 +3,13 @@ public:
     char findTheDifference(string s, string t) {
         // 25.09.23 POTD
 
-        int d=0;
+        return accumulate(t.begin(), t.end(), 0) - accumulate(s.begin(), s.end(), 0);
+        // return d;
 
-        for(int i=0; i<s.size(); i++){
-            d -= s[i],  d += t[i];
-        }
-
-        return d==0? t.back() : d+t.back();
+        // int d=0;
+        // for(int i=0; i<s.size(); i++){
+        //     d -= s[i],  d += t[i];
+        // }
+        // return d==0? t.back() : d+t.back();
     }
 };
