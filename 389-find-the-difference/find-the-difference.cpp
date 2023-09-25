2fdf101034ae4a82e9dda924a2d2946f9f01062b
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-09-25 16:16:00 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (8.68%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -3,23 +3,41 @@ public:
     char findTheDifference(string s, string t) {
         // 25.09.23 POTD
 
-        unordered_map<char,int> fr;
-        char ans='0';
+        int d=0;
 
         for(int i=0; i<s.size(); i++){
-            fr[s[i]]--;
-            fr[t[i]]++;
+            d -= s[i];
+            d += t[i];
         }
+        d += t.back();
 
-        fr[t.back()]++;
-
-        for(auto it: fr){
-            if(it.second==1){
-                ans=it.first;
-                break;
-            }
-        }
-
-        return ans=='0'? t.back(): ans;;
+        return d;
     }
 };
+
+
+// class Solution {
+// public:
+//     char findTheDifference(string s, string t) {
+//         // 25.09.23 POTD
+
+//         unordered_map<char,int> fr;
+//         char ans='0';
+
+//         for(int i=0; i<s.size(); i++){
+//             fr[s[i]]--;
+//             fr[t[i]]++;
+//         }
+
+//         fr[t.back()]++;
+
+//         for(auto it: fr){
+//             if(it.second==1){
+//                 ans=it.first;
+//                 break;
+//             }
+//         }
+
+//         return ans=='0'? t.back(): ans;;
+//     }
+// };
