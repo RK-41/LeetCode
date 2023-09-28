693d3ac3188d658cba4155fb93c5d6ce81780241
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-09-28 17:23:20 +0530

    Time: 11 ms (24.58%) | Memory: 16.6 MB (25.63%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -3,23 +3,26 @@ public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         // 28.09.23 POTD
 
-        // int i=0, j=nums.size()-1;
+        // APPROACH 1
+        int i=0, j=nums.size()-1;
 
-        // while(true){
-        //     while(i<nums.size()-1 and nums[i]%2==0) i++;
-        //     while(j>0 and nums[j]%2==1) j--;
+        while(true){
+            while(i<nums.size()-1 and nums[i]%2==0) i++;
+            while(j>0 and nums[j]%2==1) j--;
 
-        //     if(i<j) swap(nums[i], nums[j]);
-        //     else    break;
+            if(i<j) swap(nums[i++], nums[j--]);
+            else    break;
+        }
+
+        // Approach 2
+        // int j=0; 
+        // for(int i=0; i<nums.size(); i++){
+        //     if(nums[i]%2==0){
+        //         swap(nums[i], nums[j]);
+        //         j++;
+        //     }
         // }
 
-        int j=0; 
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]%2==0){
-                swap(nums[i], nums[j]);
-                j++;
-            }
-        }
         return nums;
     }
 };
