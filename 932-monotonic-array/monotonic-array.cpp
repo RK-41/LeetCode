a71ec9e9f04456ca35c6aff84f377b5eeb83d9c8
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-09-29 15:34:10 +0530

    Time: 134 ms (20.56%) | Memory: 96.8 MB (13.85%) - LeetSync

--- 932-monotonic-array/monotonic-array.cpp
+++ 932-monotonic-array/monotonic-array.cpp
@@ -3,23 +3,16 @@ public:
     bool isMonotonic(vector<int>& nums) {
         // 29.09.23 POTD
 
-        int count=1;
+        int inc=1, dec=1;;
 
         for(int i=1; i<nums.size(); i++){
             if(nums[i-1]<=nums[i])
-                count++;
-        }
-
-        if(count==nums.size())  return true;
-
-        count=1;
-
-        for(int i=1; i<nums.size(); i++){
+                inc++;
             if(nums[i-1]>=nums[i])
-                count++;
+                dec++;
         }
 
-        if(count==nums.size())  return true;
+        if(inc==nums.size() || dec==nums.size())    return true;
 
         return false;
     }
