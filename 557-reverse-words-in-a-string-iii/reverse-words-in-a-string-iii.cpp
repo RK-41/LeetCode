4473ef8b2a22f05cdf209426563bfc7a1f591957
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-10-01 14:21:37 +0530

    Time: 7 ms (98.22%) | Memory: 12.6 MB (27.43%) - LeetSync

---
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        // 01.10.23 POTD
+        stack<string> st;
+        string t;
+
+        for(int i=s.size()-1; i>=0; i--){
+            if(s[i]==' '){
+                st.push(t);
+                t="";
+            }
+            else{
+                t += s[i];
+            }
+        }
+
+        st.push(t);
+
+        t="";
+
+        while(st.size()){
+            t += st.top();
+            st.pop();
+
+            if(st.size())   t+=" ";
+        }
+
+        return t;
+    }
+};
