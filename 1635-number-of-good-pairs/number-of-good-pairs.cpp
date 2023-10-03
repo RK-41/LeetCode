f740da587791d1623f67268ccb77aba2ccdd55cc
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-10-03 18:30:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (27.67%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -4,14 +4,6 @@ public:
         // 03.10.23 POTD
 
         int ans=0;
-        // for(int i=0; i<nums.size()-1; i++){
-        //     for(int j=i+1; j<nums.size(); j++)
-        //         if(nums[i] == nums[j])
-        //             ans++;
-        // }
-
-        // return ans;
-
         unordered_map<int,int> fr;
         fr[nums[0]]=1;
 
