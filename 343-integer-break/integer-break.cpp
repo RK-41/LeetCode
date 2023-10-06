6f8f84783b429f816823568276774654644e99b3
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-10-06 16:21:30 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (36.38%) - LeetSync

---
+++ 343-integer-break/integer-break.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        // 06.10.23 
+
+        if(n==2)    return 1;
+        if(n==3)    return 2;
+        if(n==4)    return 4;
+
+        int ans = 1;
+
+        while(n>4){
+            ans *= 3;
+            n -= 3;
+        }
+
+        ans *= n;
+
+        return ans;
+    }
+};
