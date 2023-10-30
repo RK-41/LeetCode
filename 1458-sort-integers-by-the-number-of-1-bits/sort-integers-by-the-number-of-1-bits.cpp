2c379a4179a01adcc8d68f3fd5f68d987a2f7ea9
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-10-30 23:18:56 +0530

    Time: 3 ms (96.58%) | Memory: 10.5 MB (53.39%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        // 30.10.23 POTD
+        // 30.10.23 potd
+
+        // APPROACH 1:
+        // vector<int> ans;
+        // unordered_map<int, vector<int>> um;
+        // int mx = INT_MIN;
+
+        // for(auto a: arr){
+        //     um[__builtin_popcount(a)].push_back(a);
+        //     mx = max(mx, a);
+        // }
+
+        // for(int i=0; i<=log2(mx); i++){
+        //     sort(um[i].begin(), um[i].end());
+        //     ans.insert(ans.end(), um[i].begin(), um[i].end());
+        // }
+
+        // return ans;
 
 
-        vector<int> ans;
-        unordered_map<int, vector<int>> um;
-        int mx = INT_MIN;
+        // Approach 2:
+        int n = arr.size();
+        vector<int> res(n);
 
-        for(auto a: arr){
-            um[__builtin_popcount(a)].push_back(a);
-            mx = max(mx, a);
+        for(int i=0; i<n; i++)
+            res[i] = __builtin_popcount(arr[i])*10001 + arr[i];
+
+        sort(res.begin(), res.end());
+
+        for(int i=0; i<n; i++){
+            res[i] %= 10001;
         }
 
-        for(int i=0; i<=log2(mx); i++){
-            sort(um[i].begin(), um[i].end());
-            ans.insert(ans.end(), um[i].begin(), um[i].end());
-        }
-
-        return ans;
+        return res;
     }
 };
-        // sort(arr.begin(), arr.end(), [](int a, int b){
-        //    return __builtin_popcount(a) < __builtin_popcount(b);
-        // });
