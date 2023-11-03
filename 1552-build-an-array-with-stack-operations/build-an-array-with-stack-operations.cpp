6279193569317132d1eb442b271ca8bb05acd3da
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-11-03 20:40:59 +0530

    Time: 4 ms (39.00%) | Memory: 8 MB (75.36%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
@@ -2,19 +2,25 @@ class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
         // 03.11.23 POTD
-
+        // APPROACH: Considering an abstract stack having operations 'Push' and 'Pop'
+        
         vector<string> ans;
-        int j=-1;
+        int stackSize = -1;
 
-        for(int i=1; i<=n; i++){
+        // Iterating the stream of integers in the range [1, n]
+        for(int curr_num=1; curr_num<=n; curr_num++){
+            // Pushing current number to the abstract stack
             ans.push_back("Push");
-            j++;
+            stackSize++;
 
-            if(i == target.back())   break;
+            // If current number equals target's last element, the required operations are done
+            if(curr_num == target.back())   break;
 
-            if(i != target[j]){
+            // If top of stack (current number) isn't equal to the target vector's
+            //  current element, pop the abstract stack
+            if(curr_num != target[stackSize]){
                 ans.push_back("Pop");
-                j--;
+                stackSize--;
             }
         }
 
