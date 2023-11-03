d2474d5e8f189d2af21f8adeddec3f6ccc7748b0
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-11-03 20:32:42 +0530

    Time: 4 ms (39.00%) | Memory: 8.2 MB (49.67%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
@@ -2,24 +2,38 @@ class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
         // 03.11.23 POTD
-        
+
         vector<string> ans;
-        vector<int> stk;
-        int j=0;
+        // vector<int> stk;
+        int j=-1;
+
+        // for(int i=1; i<=n; i++){
+        //     stk.push_back(i);
+        //     ans.push_back("Push");
+
+        //     if(stk == target)   break;
+
+        //     if(stk[j] != target[j]){
+        //         stk.pop_back();
+        //         ans.push_back("Pop");
+        //         j--;
+        //     }
+
+        //     j++;
+        // }
 
         for(int i=1; i<=n; i++){
-            stk.push_back(i);
+            // stk.push_back(i);
             ans.push_back("Push");
+            j++;
 
-            if(stk == target)   break;
+            if(i == target.back())   break;
 
-            if(stk[j] != target[j]){
-                stk.pop_back();
+            if(i != target[j]){
+                // stk.pop_back();
                 ans.push_back("Pop");
                 j--;
             }
-
-            j++;
         }
 
         return ans;
