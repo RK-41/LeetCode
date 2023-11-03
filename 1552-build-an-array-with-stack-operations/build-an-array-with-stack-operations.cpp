04bf9667543bb4500f02231c13e1d2d7f4741fa7
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-11-03 20:33:20 +0530

    Time: 4 ms (39.00%) | Memory: 8 MB (75.36%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
@@ -4,33 +4,15 @@ public:
         // 03.11.23 POTD
 
         vector<string> ans;
-        // vector<int> stk;
         int j=-1;
 
-        // for(int i=1; i<=n; i++){
-        //     stk.push_back(i);
-        //     ans.push_back("Push");
-
-        //     if(stk == target)   break;
-
-        //     if(stk[j] != target[j]){
-        //         stk.pop_back();
-        //         ans.push_back("Pop");
-        //         j--;
-        //     }
-
-        //     j++;
-        // }
-
         for(int i=1; i<=n; i++){
-            // stk.push_back(i);
             ans.push_back("Push");
             j++;
 
             if(i == target.back())   break;
 
             if(i != target[j]){
-                // stk.pop_back();
                 ans.push_back("Pop");
                 j--;
             }
