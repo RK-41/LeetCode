53d0396b50266aa68ab2939c9cd3edf8bc15ebe4
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-01-01 00:03:00 +0530

    Time: 8 ms (5.74%) | Memory: 7 MB (7.48%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -3,7 +3,7 @@ public:
     int maxLengthBetweenEqualCharacters(string s) {
         // 31.12.23 POTD
         int ans=0;
-        unordered_map<char,int> first, last, count;
+        unordered_map<char,int> count, first, last;
         for(int i=0; i<s.size(); i++){
             count[s[i]]++;
             if(count[s[i]]==1){
