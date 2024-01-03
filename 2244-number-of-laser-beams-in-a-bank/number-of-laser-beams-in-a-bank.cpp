1b910518a88017714cc8ef067b57e1aefc37e7f0
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-01-03 17:54:26 +0530

    Time: 1214 ms (5.06%) | Memory: 23.3 MB (96.49%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -6,7 +6,8 @@ public:
 
         for(int i=0; i<bank.size(); i++){
             for(int j=0; j<bank[i].size(); j++){
-                curr += (bank[i][j]-'0');
+                // curr += (bank[i][j]-'0');
+                curr = accumulate(bank[i].begin(), bank[i].end(), -1*('0'*bank[i].size()));
             }
 
             if(prev>0 and curr>0){
