4dbce6c2937f19d3313f04c38b77181ff3348cc8
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-01-22 23:07:37 +0530

    Time: 15 ms (97.34%) | Memory: 23.8 MB (36.81%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> findErrorNums(vector<int>& nums) {
         // 22.01.24 POTD
         vector<int> ans;
-        int idealSum=0, actualSum=0;
+        int idealSum=0, actualSum=accumulate(nums.begin(), nums.end(), 0);
 
         for(int i=0; i<nums.size(); i++){
             if(nums[abs(nums[i])-1] < 0)
@@ -11,7 +11,6 @@ public:
 
             nums[abs(nums[i])-1] *= (-1);
             idealSum += (i+1);
-            actualSum += abs(nums[i]);
         }
 
         int missing = idealSum - (actualSum - ans[0]);
