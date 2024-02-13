a6e0a0782e6ee5c00687a52841250b40ff5fa72a
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-02-13 15:18:12 +0530

    Time: 42 ms (87.48%) | Memory: 28.2 MB (13.81%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -4,10 +4,13 @@ public:
         // 13.02.24 POTD
         
         for(auto w: words){
-            string s=w;
-            reverse(s.begin(), s.end());
-            if(s==w)
-                return s;
+            int n=w.size();
+            if(w[0]==w[n-1]){
+                string s=w;
+                reverse(s.begin(), s.end());
+                if(s==w)
+                    return s;
+            }
         }
 
         return "";
