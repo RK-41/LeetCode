3e6ad0291b95347aee6715ebe1e0f6fc3a8768c4
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-02-14 18:54:14 +0530

    Time: 136 ms (90.59%) | Memory: 143 MB (5.30%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         // 14.02.24 POTD
+        vector<int> pos, neg;
         int n=nums.size();
-        vector<int> ans, pos, neg;
 
         for(int i=0; i<n; i++){
             if(nums[i]<0)
@@ -12,6 +12,43 @@ public:
                 pos.push_back(i);
         }
 
+        // APPROACH I: Operating on the given vector (NOT PERFECT)
+        // int i=0, j=0, k=0;
+        // while(i<n && j<pos.size() && k<neg.size()){
+        //     if(i%2==0 && nums[i]<0){
+        //         int t=nums[i];
+        //         if(nums[neg[k]]>=0){
+        //             nums[i]=nums[neg[k]];
+        //             nums[neg[k]]=t;
+        //             k++;
+        //         } else {
+        //             nums[i]=nums[pos[j]];
+        //             nums[pos[j]]=t;
+        //             j++;
+        //         }
+        //     }
+
+        //     if(i%2==1 && nums[i]>=0){
+        //         int t=nums[i];
+        //         if(nums[pos[k]]<0){
+        //             nums[i]=nums[pos[j]];
+        //             nums[pos[j]]=t;
+        //             j++;
+        //         } else {
+        //             nums[i]=nums[neg[k]];
+        //             nums[neg[k]]=t;
+        //             k++;
+        //         }
+        //     }
+            
+        //     i++;
+
+        // }
+
+        // return nums;
+
+        // APPROACH II: Using another vector
+        vector<int> ans;
         for(int i=0; i<n/2; i++){
             ans.push_back(nums[pos[i]]);
             ans.push_back(nums[neg[i]]);
