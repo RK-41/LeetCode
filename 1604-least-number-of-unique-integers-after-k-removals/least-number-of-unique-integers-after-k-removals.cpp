83ebbf5065288607ddb6710ba6abfce23851abd8
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-02-16 22:48:44 +0530

    Time: 132 ms (63.73%) | Memory: 68.6 MB (31.52%) - LeetSync

---
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        // 16.02.24 POTD
+        if(k == arr.size())
+            return 0;
+
+        unordered_map<int,int> fr;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+
+        for(auto a: arr){
+            fr[a]++;
+        }
+
+        for(auto p: fr){
+            pq.push({p.second, p.first});
+        }
+
+        while(k>0){
+            k -= pq.top().first;
+            
+            if(k>=0)
+                pq.pop();
+        }
+
+        return pq.size();
+
+    }
+};
