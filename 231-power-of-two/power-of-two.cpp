73a206bb87977ab45c1c6c008ce2d97f6ac04104
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-02-19 20:56:42 +0530

    Time: 2 ms (39.05%) | Memory: 7.2 MB (19.05%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         // 19.02.24 POTD
-        if(n == 1)    return true;
-        if(n == 0 || n%2 == 1)    return false;
-
         int pc = __builtin_popcount(n);
+
         return pc>1 ? false : n>0 ? true : false;
     }
 };
