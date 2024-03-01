e79443629be6a1b5b3d7c37f8760b1dd84f45748
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-03-02 00:00:30 +0530

    Time: 3 ms (77.44%) | Memory: 9.1 MB (19.34%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -3,14 +3,18 @@ public:
     string maximumOddBinaryNumber(string s) {
         // 01.03.24 POTD
         string ans="";
-        for(int i=0; i<s.size(); i++){
-            if(s[i]=='1')
-                ans = "1"+ans;
-            else
-                ans = ans+"0";
-        }
+        // for(int i=0; i<s.size(); i++){
+        //     if(s[i]=='1')
+        //         ans = "1"+ans;
+        //     else
+        //         ans = ans+"0";
+        // }
+        // ans = ans.substr(1) + "1";
 
-        ans = ans.substr(1) + "1";
-        return ans;
+        sort(s.begin(), s.end());
+        reverse(s.begin(), s.end());
+
+        s = s.substr(1) + "1";
+        return s;
     }
 };
