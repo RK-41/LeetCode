2bc08cb2874781e690c6677df3c5499cd31ea977
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-03-06 00:13:13 +0530

    Time: 18 ms (97.59%) | Memory: 13.9 MB (70.34%) - LeetSync

---
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        // 05.03.24 POTD
+        int n=s.size();
+        if(n==1)    return 1;
+        
+        if(s[0]!=s[n-1])    return n;
+
+        int i=0, j=n-1;
+        while(i<j){
+            if(s[i]!=s[j])
+                break;
+
+            char c=s[i];
+            while(s[i]==c && i<=j)
+                i++;
+            while(s[j]==c && j>i)
+                j--;
+        }
+
+        if(i==j && s[i-1]==s[j+1])
+            return !(s[i-1]==s[i]);
+
+        return j<i ? 0 : j-i+1;
+    }
+};
