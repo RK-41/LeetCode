75e060e87999850cb6e7c9a514fba4d290676beb
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-04-07 21:25:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (99.47%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        // 07.04.24
-        int left=0, right=0, ast=0, last=0;
-        int cnt=0;
+        // 07.04.24 POtd
+        int left=0, right=0, ast=0, cnt=0;
+
         for(char c: s){
             if(c=='('){
                 left++;
-                last=-1;
                 cnt++;
             }  
             else if(c==')'){
                 right++;
-                last=1;
                 cnt--;
             }
             else{
                 ast++;
-                last=0;
                 cnt--;
-                cout<<"*";
             }
 
             if(right > left+ast)
                 return false;
 
             cnt = max(cnt, 0);
-            cout<<cnt<<" ";
         }
-
-        // if(last==-1)
-        //     return false;
 
         return cnt==0;
     }
