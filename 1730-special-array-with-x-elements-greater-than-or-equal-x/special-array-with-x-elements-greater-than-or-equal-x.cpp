62e654fb9d4ec271ef5030884b2cd1f152576abb
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-05-27 21:38:40 +0530

    Time: 4 ms (36.86%) | Memory: 11.7 MB (5.12%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int specialArray(vector<int>& nums) {
         // 27.05.24 POTD
-        int ans = nums.size();
-        int mn = *min_element(nums.begin(), nums.end());
+        // int ans = nums.size();
+        // int mn = *min_element(nums.begin(), nums.end());
 
-        if(ans <= mn)
-            return ans;
+        // if(ans <= mn)
+        //     return ans;
 
-        int mx = *max_element(nums.begin(), nums.end());
-        if(mx == 0)
-            return -1;
+        // int mx = *max_element(nums.begin(), nums.end());
+        // if(mx == 0)
+        //     return -1;
         
         unordered_map<int,int> um;
         for(int i=1; i<=100; i++){
