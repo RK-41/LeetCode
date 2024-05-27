1b35873825927eadb5982413b85d745fa2c4074f
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-05-27 21:38:10 +0530

    Time: 10 ms (5.29%) | Memory: 11.7 MB (5.12%) - LeetSync

---
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+        // 27.05.24 POTD
+        int ans = nums.size();
+        int mn = *min_element(nums.begin(), nums.end());
+
+        if(ans <= mn)
+            return ans;
+
+        int mx = *max_element(nums.begin(), nums.end());
+        if(mx == 0)
+            return -1;
+        
+        unordered_map<int,int> um;
+        for(int i=1; i<=100; i++){
+            for(auto e: nums){
+                if(i <= e){
+                    um[i]++;
+                }
+            }
+
+            if(i == um[i])
+                return i;
+        }
+
+        return -1;
+    }
+};
