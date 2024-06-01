1dace9fe42865d904a731ffff1706d1a02a53243
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-06-01 15:23:35 +0530

    Time: 4 ms (38.06%) | Memory: 8.2 MB (93.20%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,54 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        // 21.05.24 POTD
-        vector<vector<int>> subsets = {{}};
-        queue<vector<int>> q;
+        // 01.06.24
+        vector<vector<int>> subsets;
         int n = nums.size();
+        int total = 1<<n;
 
-        for(int a: nums){
-            subsets.push_back({a});
-        }        
-
-        int i=0;
-        while(i<n){
-            int sz = q.size();
-
-            while(sz--){
-                auto f = q.front();
-                q.pop();
-                q.push(f);
-
-                f.push_back(nums[i]);
-                q.push(f);
-
-                subsets.push_back(f);
+        for(int i=0; i<total; i++){
+            vector<int> sub;
+            for(int j=0; j<n; j++){
+                if(i & (1<<j)){
+                    sub.push_back(nums[j]);
+                }
             }
-            q.push({nums[i]});
-            i++;
+
+            subsets.push_back(sub);
         }
-        
+
         return subsets;
     }
 };
-
-/*
-Visualizing subset creation as nums array is iterated from start to end:
-
-a1
-
-a2
-a1,a2
-
-a3
-a1,a3
-a2,a3
-a1,a2,a3
-
-a4
-a1,a3,a4
-a2,a3,a4
-a1,a2,a3,a4
-
-
-*/
