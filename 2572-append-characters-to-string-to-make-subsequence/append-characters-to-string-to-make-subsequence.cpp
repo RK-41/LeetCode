6814320552c8552fc4ccdf70220639ed342d20cb
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-06-03 21:56:56 +0530

    Time: 23 ms (20.38%) | Memory: 12.1 MB (11.40%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int appendCharacters(string s, string t) {
-        // 03.06.24
+        // 03.06.24 POTD
         int ans=0, i=0, j=0, n=s.size(), m=t.size();
 
         while(i<n && j<m){
