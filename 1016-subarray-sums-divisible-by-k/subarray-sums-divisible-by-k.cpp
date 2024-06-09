a880c7495d56565c87e75e3731ba3e553ba17e42
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-06-09 22:21:31 +0530

    Time: 38 ms (68.73%) | Memory: 35.2 MB (18.10%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -17,12 +17,12 @@ public:
                 mod += k;
             }
 
-            if(cumMap.count(mod)){
+            // if(cumMap.count(mod)){
                 count += cumMap[mod];
                 cumMap[mod] += 1;
-            } else {
-                cumMap[mod] = 1;
-            }
+            // } else {
+                // cumMap[mod] = 1;
+            // }
         }
 
         return count;
