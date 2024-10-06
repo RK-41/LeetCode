30247e7c1a5001a7fadd1047361a17e579c9b5d4
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-10-06 23:52:19 +0530

    Time: 5 ms (8.76%) | Memory: 8.4 MB (66.36%) - LeetSync

---
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool areSentencesSimilar(string sentence1, string sentence2) {
+        // 06.10.24 Potd
+        auto splitWords = [](const string& sentence){
+            vector<string> words;
+            string s;
+            for(auto c: sentence){
+                if(c == ' '){
+                    if(!s.empty()){
+                        words.push_back(s);
+                        s = "";
+                    }
+                } else {
+                    s += c;
+                }
+            }
+            if(!s.empty())
+                words.push_back(s);
+
+            return words;
+        };
+        
+        vector<string> words1 = splitWords(sentence1);
+        vector<string> words2 = splitWords(sentence2);
+        if(words1.size() < words2.size())   swap(words1, words2);
+
+        int start = 0, end = 0, n1 = words1.size(), n2 = words2.size();
+
+        while(start < n2 && words1[start] == words2[start])  start++;
+        while(end < n2 && words1[n1-1-end] == words2[n2-1-end]) end++;
+
+        return start + end >= n2;
+    }
+};
