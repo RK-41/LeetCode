08692ce00246758c7a7afac974785af72378bad5
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-12-13 00:18:26 +0530

    Time: 4 ms (62.99%) | Memory: 17.4 MB (20.60%) - LeetSync

---
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.py
@@ -0,0 +1,15 @@
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        # 12.12.24 POTD
+        heap = []
+        heapify(heap)
+
+        for g in gifts:
+            heappush(heap, -1 * g)
+
+        while k>0 and len(heap):
+            mx = -1 * heappop(heap)
+            heappush(heap, -1 * floor(sqrt(mx)))
+            k -= 1
+
+        return -1 * sum(heap)
