fb9a5c9604f05373b91c2676bccf2ed3914b6117
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-12-15 01:12:37 +0530

    Time: 599 ms (84.80%) | Memory: 27.3 MB (50.42%) - LeetSync

---
+++ 2868-continuous-subarrays/continuous-subarrays.py
@@ -0,0 +1,22 @@
+class Solution:
+    def continuousSubarrays(self, nums: List[int]) -> int:
+        # 14.12.24
+        ans, left, n = 0, 0, len(nums)
+        minD, maxD = deque(), deque()
+
+        for right in range(n):
+            while minD and nums[minD[-1]] >= nums[right]: minD.pop()
+            while maxD and nums[maxD[-1]] <= nums[right]: maxD.pop()
+            minD.append(right)
+            maxD.append(right)
+
+            while nums[maxD[0]] - nums[minD[0]] > 2:
+                left += 1
+                if minD[0] < left: minD.popleft()
+                if maxD[0] < left: maxD.popleft()
+
+            ans += right - left + 1
+
+        return ans
+
+        
