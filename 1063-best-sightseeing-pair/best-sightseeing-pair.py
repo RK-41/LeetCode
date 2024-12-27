099a28955d9b52cac25f2ebc8ac6858fb5f7dd1a
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-12-28 02:30:18 +0530

    Time: 82 ms (21.96%) | Memory: 23 MB (33.41%) - LeetSync

---
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        # 27.12.24 POTD
+        ans = 0
+        n = len(values)
+        maxValJRL = [0] * n # max value[j] - j right to left
+        maxValJRL[n-1] = values[n-1] - (n-1)
+
+        # Calculating max value of (values[j] - j) at ith position
+        for i in range(n-2,-1,-1):
+            maxValJRL[i] = max(values[i] - i, maxValJRL[i+1])
+
+        # Finding max score
+        for i in range(n-1):
+            ans = max(ans, values[i] + i + maxValJRL[i+1])
+
+        return ans
