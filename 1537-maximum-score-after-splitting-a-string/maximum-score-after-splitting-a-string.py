c9e29bdbc4948d41ecfab968d1ea614937bb294d
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2025-01-01 16:05:16 +0530

    Time: 3 ms (57.89%) | Memory: 17.7 MB (15.52%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
@@ -2,16 +2,16 @@ class Solution:
     def maxScore(self, s: str) -> int:
         # 01.01.25 POTD
         ans = 0
-        n = len(s)
-        onesRTL = [0] * n
-        onesRTL[n-1] = 1 if s[n-1] == '1' else 0
+        ones = 0
         zeros = 1 if s[0] == '0' else 0
+        n = len(s)
 
-        for i in range(n-2,-1,-1):
-            onesRTL[i] = onesRTL[i+1] + (1 if s[i] == '1' else 0)
+        for i in range(n-1,0,-1):
+            ones += int(s[i])
 
         for i in range(1,n):
-            ans = max(ans, zeros + onesRTL[i])
+            ans = max(ans, zeros + ones)
             zeros += 1 if s[i] == '0' else 0
+            ones -= int(s[i])
 
         return ans
