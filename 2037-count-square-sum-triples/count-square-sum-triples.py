fe2639ef4b5b5ea02efe97c148749ea49d1a2bcf
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2025-12-08 23:54:45 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.15%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countTriples(self, n: int) -> int:
+        # 08.12.25 potd
+        cnt=0
+        nsqrt=int(sqrt(n))
+        for s in range(2, nsqrt+1):
+            for t in range((s&1)+1, s, 2):
+                if gcd(s,t)!=1: continue
+                c=s*s+t*t
+                if c>n: break
+                k=n//c
+                cnt+=2*k
+        return cnt
+        
