1633636a286475e21083c0fddcf7dafd3e886363
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2026-04-26 13:05:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (42.56%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        // 21.09.23 (potd)
-        if(nums.size()==1)
-            return nums[0];
-
+        // 26.04.26
         int n = nums.size();
-        nums[n-2] = max(nums[n-1],nums[n-2]);
+        if(n==1)    return nums[0];
 
-        // Traversing 'nums' from end to beginning and,
-        // finding max amount of money between (current house plus current+2 house) and (current+1 house)
-        // and storing it in the current house
-        for(int i{n-3}; i>=0; i--){
-            nums[i] = max(nums[i]+nums[i+2], nums[i+1]);
+        vector<int> dp(n+1, 0);
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i{2}; i<=n; i++){
+            int steal = nums[i-1] + dp[i-2];
+            int skip = dp[i-1];
+            dp[i] = max(steal, skip);
         }
 
-        // The first house would have the max amount of money
-        return nums[0];
+        return dp[n];
     }
 };
